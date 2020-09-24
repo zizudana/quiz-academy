a8b42ead2582a8ced786793f465a0b1a422db8ba
sehan <sehandev@gmail.com>
2020-09-24 23:11:17 +0900

    EDIT index

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ const IndexPage = () => {
   return (
     <>
       <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">Next.js + Tailwind CSS</h1>
-        <img src="/img/test.jpg" alt="Girl in a jacket" width="500" height="600" />
+        <h1 className="text-5xl text-center text-accent-1">대치동 온라인</h1>
       </div>
     </>
   )

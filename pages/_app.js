408ff4a3c111fe70b5eb12b6831873f90dd21480
sehandev <sehandev@gmail.com>
2020-09-24 22:00:14 +0900

    ADD font, container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Head from "next/head"
+import Nav from "../components/nav"
 import "../styles/index.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>대치동 온라인</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto px-5 mb-5">
+        {/* START : container */}
+
+        {/* Navigation Bar */}
+        <Nav />
+
+        <Component {...pageProps} />
+
+        {/* END : container */}
+      </div>
+    </>
+  )
 }
 
 export default MyApp

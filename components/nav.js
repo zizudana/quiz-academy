0f40f6d11d4543531420c5029224ebc37c0a6ecd
sehandev <sehandev@gmail.com>
2020-09-28 00:53:27 +0900

    EDIT nav.js : logo, Link, login

--- components/nav.js
+++ components/nav.js
@@ -3,44 +3,46 @@ import Link from "next/link"
 const Nav = () => {
   return (
     <>
-      <div className="color-2 overflow-hidden text-large text-white h-40px px-6 py-2">
-        <div className="float-left leading-8">
-          <Link href="/">
-            <a className="header-kind no-underline px-2 py-0">대치동 온라인</a>
-          </Link>
-        </div>
-        <div className="float-right leading-8">
-          <input
-            className="hidden md:inline-block border-2 border-gray-300 bg-white h-6 px-3 mr-3 rounded-lg text-sm focus:outline-none"
+      <div className="color-2 overflow-hidden text-lg text-white h-20 px-6 py-2 flex items-center justify-between">
+        <Link href="/">
+          <a className="flex items-center">
+            <img src="/img/DCD_logo.png" alt="logo" className="w-8 mr-2" />
+            <span className="block tracking-widest text-white text-2xl font-bold">DCD On</span>
+          </a>
+        </Link>
+        <div>
+          {/* <input
+            className="hidden sm:inline-block border-2 border-gray-300 bg-white h-6 px-3 mr-3 rounded-lg text-sm focus:outline-none"
             type="search"
             placeholder="검색"
-          />
+          /> */}
           <Link href="/pdf">
-            <a className="hidden md:inline-block header-kind no-underline px-2 py-0">문제풀이</a>
+            <a className="hidden sm:inline-block px-2 py-0">문제풀이</a>
           </Link>
-          <a href="#contact" className="hidden md:inline-block header-kind no-underline px-2 py-0">
-            오답노트
-          </a>
-          <a href="#about" className="hidden md:inline-block header-kind no-underline px-2 py-0">
-            질의응답
-          </a>
-          <button onClick={() => ShowHiddenHeader()} className="md:hidden color-3 text-white font-bold rounded align-middle w-6 h-6">
+          <Link href="/">
+            <a className="hidden sm:inline-block px-2 py-0">오답노트</a>
+          </Link>
+          <Link href="/">
+            <a className="hidden sm:inline-block px-2 py-0">질의응답</a>
+          </Link>
+          <Link href="/login">
+            <a className="inline-block bg-indigo-100 hover:bg-indigo-300 text-gray-800 font-bold py-1 px-4 ml-3 rounded-full">로그인</a>
+          </Link>
+          <button onClick={() => ShowHiddenHeader()} className="sm:hidden color-3 text-white font-bold leading-4 rounded align-middle w-6 h-6">
             =
           </button>
         </div>
       </div>
-      <div id="hidden-header" className="hidden md:hidden px-6 color-3">
+      <div id="hidden-header" className="hidden sm:hidden px-6 color-3">
         <Link href="/pdf">
-          <a href="#news" className="block header-kind no-underline px-2 py-1 hover:bg-indigo-200 rounded">
-            문제풀이
-          </a>
+          <a className="block px-2 py-2 font-bold text-white hover:bg-indigo-200 rounded">문제풀이</a>
         </Link>
-        <a href="#contact" className="mt-1 block header-kind no-underline px-2 py-1 hover:bg-indigo-200 rounded">
-          오답노트
-        </a>
-        <a href="#about" className="mt-1 block header-kind no-underline px-2 py-1 hover:bg-indigo-200 rounded">
-          질의응답
-        </a>
+        <Link href="/">
+          <a className=" block px-2 py-2 font-bold text-white hover:bg-indigo-200 rounded">오답노트</a>
+        </Link>
+        <Link href="/">
+          <a className=" block px-2 py-2 font-bold text-white hover:bg-indigo-200 rounded">질의응답</a>
+        </Link>
       </div>
 
       <script src="/js/nav.js"></script>

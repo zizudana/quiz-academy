9cc32f045110c54f3352a882da9b91cf1c09de5a
sehandev <sehandev@gmail.com>
2020-09-28 00:53:59 +0900

    EDIT _app.js : navbar를 container 밖으로

--- pages/_app.js
+++ pages/_app.js
@@ -12,11 +12,11 @@ const MyApp = ({ Component, pageProps, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto px-5 mb-5">
+      <Nav />
+      <div className="container mx-auto mb-5">
         {/* START : container */}
 
         {/* Navigation Bar */}
-        <Nav />
 
         <motion.div
           key={router.route}

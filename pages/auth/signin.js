d0b3b0919e29ab9e5fc52ea55faedf0f860d27dc
sehandev <sehandev@gmail.com>
2020-10-02 16:13:38 +0900

    EDIT : signin page css

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,23 +1,35 @@
-import Layout from "../../components/layout"
 import React from "react"
 import { csrfToken } from "next-auth/client"
 
 export default function SignIn({ csrfToken }) {
   return (
-    <Layout>
+    <div className="w-full max-w-xs mt-8 m-auto bg-indigo-100 rounded p-5">
+      <img src="/img/DCD_logo.png" alt="logo" className="w-32 mx-auto" />
       <form method="post" action="/api/auth/callback/credentials">
-        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-        <label>
-          Username
-          <input name="username" type="text" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="text" />
-        </label>
-        <button type="submit">Sign in</button>
+        <div>
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <label className="block mb-2 text-indigo-500" htmlFor="username">
+            Username
+          </label>
+          <input className="w-full p-2 mb-6 text-indigo-700 border-b-2 bg-indigo-100 border-indigo-500 outline-none" type="text" name="username" />
+        </div>
+        <div>
+          <label className="block mb-2 text-indigo-500" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="w-full p-2 mb-6 text-indigo-700 border-b-2 bg-indigo-100 border-indigo-500 outline-none"
+            type="password"
+            name="password"
+          />
+        </div>
+        <div>
+          <button className="w-full bg-indigo-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 mb-6 rounded focus:outline-none" type="submit">
+            Sign in
+          </button>
+        </div>
       </form>
-    </Layout>
+    </div>
   )
 }
 

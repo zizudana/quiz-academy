f30b5c1d279e6306edc309a826c6203665d996a6
sehandev <sehandev@gmail.com>
2020-10-02 16:11:18 +0900

    ADD : 로그아웃 상태면 "/"로 redirect

--- components/layout.js
+++ components/layout.js
@@ -1,34 +1,57 @@
 import Head from "next/head"
 import Nav from "../components/nav"
 import { motion } from "framer-motion"
+import { useSession, getSession } from "next-auth/client"
+import { useRouter } from "next/router"
 
-const Layout = ({ children }) => (
-  <>
-    <Head>
-      <title>대치동 온라인</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Layout = ({ children }) => {
+  const [session, loading] = useSession()
 
-    <Nav />
+  if (typeof window !== "undefined" && loading) return null
 
-    <main className="container mx-auto mb-5">
-      <motion.div
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-          },
-        }}
-      >
-        {children}
-      </motion.div>
-    </main>
-  </>
-)
+  if (!session) {
+    const router = useRouter()
+    if (typeof window !== "undefined") {
+      router.push("/")
+    }
+    return <></>
+  } else {
+    return (
+      <>
+        <Head>
+          <title>대치동 온라인</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Nav />
+
+        <main className="container mx-auto mb-5">
+          <motion.div
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+          >
+            {children}
+          </motion.div>
+        </main>
+      </>
+    )
+  }
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props: { session },
+  }
+}
 
 export default Layout

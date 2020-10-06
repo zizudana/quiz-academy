f96cc4add759fa77cd9879cca9d591b7c924bcef
sehandev <sehandev@gmail.com>
2020-10-06 14:17:05 +0900

    ADD : signin error message

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { csrfToken } from "next-auth/client"
 
-export default function SignIn({ csrfToken }) {
+const SignInPage = ({ csrfToken, props }) => {
   return (
     <div className="w-full max-w-xs mt-12 m-auto bg-indigo-100 rounded p-5">
       <img src="/img/DCD_logo.png" alt="logo" className="w-32 mx-auto" />
@@ -24,6 +24,7 @@ export default function SignIn({ csrfToken }) {
           />
         </div>
         <div>
+          <p className="mb-3 text-red-600 text-base text-center">{props["error_message"]}</p>
           <button className="w-full bg-indigo-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none" type="submit">
             Sign in
           </button>
@@ -41,8 +42,15 @@ export default function SignIn({ csrfToken }) {
   )
 }
 
-SignIn.getInitialProps = async (context) => {
+SignInPage.getInitialProps = async (context) => {
+  let message = ""
+  if (context.query.error) {
+    message = "ID와 비밀번호를 확인해주세요"
+  }
   return {
     csrfToken: await csrfToken(context),
+    props: { error_message: message }, // will be passed to the page component as props
   }
 }
+
+export default SignInPage

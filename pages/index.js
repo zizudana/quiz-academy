184b4f223af7bf6ecf70866c3a599b4adce7e2c7
sehandev <sehandev@gmail.com>
2020-10-09 15:09:53 +0900

    EDIT : index page layout_guest 적용

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,27 @@
 import Link from "next/link"
-import { useSession, getSession } from "next-auth/client"
-import { useRouter } from "next/router"
+import { getSession } from "next-auth/client"
+import Layout from "../components/layout_guest"
 
 const IndexPage = () => {
-  const [session, loading] = useSession()
+  return (
+    <Layout>
+      <style global jsx>{`
+        body {
+          background-color: #1e1651;
+        }
+      `}</style>
+      <div className="text-white h-auto px-6 pt-10 flex flex-col">
+        <img src="/img/DCD_logo.png" alt="logo" className="w-32 mx-auto" />
+        <span className="text-5xl mb-20 font-bold text-center">DCD On</span>
 
-  if (typeof window !== "undefined" && loading) return null
-
-  if (session) {
-    if (typeof window !== "undefined") {
-      const router = useRouter()
-      router.push("/pdf")
-    }
-    return <></>
-  } else {
-    return (
-      <>
-        <div className="color-2 h-screen">
-          <div className="container mx-auto mb-5">
-            <div className="text-white h-auto px-6 pt-10 flex flex-col">
-              <img src="/img/DCD_logo.png" alt="logo" className="w-32 mx-auto" />
-              <span className="text-5xl mb-20 font-bold text-center">DCD On</span>
-
-              <Link href="/auth/signin">
-                <a className="mx-auto mb-20 bg-indigo-100 hover:bg-indigo-300 text-gray-800 font-bold text-2xl py-1 px-4 text-center rounded-full">
-                  접속하기
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </>
-    )
-  }
+        <Link href="/auth/signin">
+          <a className="mx-auto mb-20 bg-indigo-100 hover:bg-indigo-300 text-gray-800 font-bold text-2xl py-1 px-4 text-center rounded-full">
+            접속하기
+          </a>
+        </Link>
+      </div>
+    </Layout>
+  )
 }
 
 export async function getServerSideProps(context) {

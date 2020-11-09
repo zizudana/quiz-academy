f3685aa2b1675ef3e5472271aa9a5a5ed3c95d52
sehandev <sehandev@gmail.com>
2020-11-09 17:27:10 +0900

    EDIT : layout_user student 아닌 경우 redirect

--- components/layout/layout_user.js
+++ components/layout/layout_user.js
@@ -1,6 +1,7 @@
 import Nav from "./nav"
 
 import Head from "next/head"
+import { useEffect } from "react"
 import { motion } from "framer-motion"
 import { useSession } from "next-auth/client"
 import { useRouter } from "next/router"
@@ -8,46 +9,51 @@ import { useRouter } from "next/router"
 const Layout = ({ children }) => {
   const [session, loading] = useSession()
 
-  if (typeof window !== "undefined" && loading) {
-    return null
-  }
+  const router = useRouter()
 
-  if (!session) {
-    const router = useRouter()
-    if (typeof window !== "undefined") {
+  useEffect(() => {
+    if (!session) {
+      // guest 계정
+      router.push("/")
+    } else if (session.user.email !== "student") {
+      // student가 아닌 다른 계정
       router.push("/")
     }
+  }, [session])
+
+  if (loading) {
     return null
-  } else {
-    return (
-      <>
-        <Head>
-          <title>대치동 온라인</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <Nav />
-
-        <main className="container mx-auto mb-5">
-          <motion.div
-            initial="pageInitial"
-            animate="pageAnimate"
-            variants={{
-              pageInitial: {
-                opacity: 0,
-              },
-              pageAnimate: {
-                opacity: 1,
-              },
-            }}
-          >
-            {children}
-          </motion.div>
-        </main>
-      </>
-    )
   }
+
+  // student 계정
+  return (
+    <>
+      <Head>
+        <title>대치동 온라인</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <main className="container mx-auto mb-5">
+        <motion.div
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+        >
+          {children}
+        </motion.div>
+      </main>
+    </>
+  )
 }
 
 export default Layout

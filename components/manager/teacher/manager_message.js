8c5cc1397abf8df0f511fd9389371a7398f2a83b
sehandev <sehandev@gmail.com>
2020-11-10 20:07:06 +0900

    ADD js : file upload

--- components/manager/teacher/manager_message.js
+++ components/manager/teacher/manager_message.js
@@ -3,8 +3,8 @@ import { useState } from "react"
 const axios = require("axios")
 
 const ManagerMessage = ({ rest_api_url }) => {
-  const [message_title, set_message_title] = useState("")
-  const [message_content, set_message_content] = useState("")
+  const [message_title, set_message_title] = useState("") // 관리자 메세지 제목
+  const [message_content, set_message_content] = useState("") // 관리자 메세지 내용
 
   const on_change_message_title = (event) => {
     set_message_title(event.target.value)
@@ -15,9 +15,55 @@ const ManagerMessage = ({ rest_api_url }) => {
   }
 
   const save_message = () => {
-    axios.post(`${rest_api_url}/managermessages`, { title: message_title, content: message_content, filepath: "/tmp/binasdf/" }).then((response) => {
-      console.log(response)
-    })
+    if (message_title === "" || message_content === "") {
+      alert("제목과 내용을 모두 작성해주세요.")
+    } else {
+      axios
+        .post(`${rest_api_url}/managermessages`, { title: message_title, content: message_content })
+        .then((response) => {
+          // 관리자 메세지 제목, 내용 저장 성공
+
+          console.log(response)
+
+          var screenshot_array = document.querySelector("#screenshot-input").files
+
+          // screenshot_array file post
+          if (0 < screenshot_array.length) {
+            // screenshot 저장 경로 : /files/managermessages/[관리자 메세지 ObjectID]
+            let filepath = `managermessages/${response.data.InsertedID}/`
+
+            // path 마지막은 slash(/)로 통일
+            if (filepath.slice(-1) !== "/") {
+              filepath += "/"
+            }
+
+            // 파일 전송을 그대로 하기 위해 formData 사용
+            var formData = new FormData()
+            formData.append("filepath", filepath)
+            Array.from(screenshot_array).forEach((file) => {
+              formData.append("files", file)
+            })
+
+            // content-type 설정으로 file 전송 가능
+            axios
+              .post(`${rest_api_url}/managermessages/files`, formData, {
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                },
+              })
+              .then((response) => {
+                console.log(response)
+              })
+          }
+        })
+        .catch((error) => {
+          alert(error)
+        })
+
+      // 관리자 메세지 저장 후 제목과 내용 초기화
+      set_message_title("")
+      set_message_content("")
+    }
   }
 
   return (
@@ -64,7 +110,13 @@ const ManagerMessage = ({ rest_api_url }) => {
         />
 
         {/* 스크린샷 첨부 */}
-        <input type="file" multiple className="w-full px-4 py-2 mb-3 bg-indigo-300 text-gray-100 text-sm rounded focus:outline-none" />
+        <input
+          id="screenshot-input"
+          type="file"
+          multiple
+          accept=".jpg,.jpeg,.gif,.png"
+          className="w-full px-4 py-2 mb-3 bg-indigo-300 text-gray-100 text-sm rounded focus:outline-none"
+        />
 
         {/* save button */}
         <button

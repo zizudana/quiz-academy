5b413e63d9bb629a84e3fadd9fb5d843e39f4da5
sehandev <sehandev@gmail.com>
2020-11-12 23:48:34 +0900

    ADD : user qna comment - DB 연결

--- components/connector/connector_bottom.js
+++ components/connector/connector_bottom.js
@@ -4,12 +4,15 @@ import QuestionHistory from "./question_history"
 import { useState, useEffect } from "react"
 const axios = require("axios")
 
-const ConnectorBottom = ({ qna_student_id, rest_api_url }) => {
-  const [qna_query_list, set_qna_query_list] = useState([]) // 학생 목록
+const ConnectorBottom = ({ qna_student_id, rest_api_url, refresh }) => {
+  const [qna_query_list, set_qna_query_list] = useState([]) // 상시 질문 목록
+  const [qna_comment_list, set_qna_comment_list] = useState([]) // 코멘트 목록
 
   useEffect(() => {
     let tmp_qna_query_list = []
+    let tmp_qna_comment_list = []
 
+    // 상시 질문 목록 불러오기
     axios
       .get(`${rest_api_url}/qnaquerys/all/qnastudentid/${qna_student_id}`)
       .then(function (response) {
@@ -24,7 +27,28 @@ const ConnectorBottom = ({ qna_student_id, rest_api_url }) => {
          * * @param {String} ischecked 확인 여부
          */
         tmp_qna_query_list = response.data
-        tmp_qna_query_list.sort((a, b) => a.timestamp - b.timestamp)
+        tmp_qna_query_list.sort((a, b) => b.timestamp - a.timestamp)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+
+    // 코멘트 목록 불러오기
+    axios
+      .get(`${rest_api_url}/qnacomments/all/qnastudentid/${qna_student_id}`)
+      .then(function (response) {
+        // handle success
+
+        /**
+         * @param {Object} qna_comment_object
+         * * @param {String} qnastudentid (부모) 질의응답+학생 ID
+         * * @param {String} title 제목
+         * * @param {String} content 내용
+         * * @param {int64} timestamp 작성시간
+         */
+        tmp_qna_comment_list = response.data
+        tmp_qna_comment_list.sort((a, b) => b.timestamp - a.timestamp)
       })
       .catch(function (error) {
         // handle error
@@ -33,14 +57,16 @@ const ConnectorBottom = ({ qna_student_id, rest_api_url }) => {
 
     setTimeout(() => {
       set_qna_query_list(tmp_qna_query_list)
+      set_qna_comment_list(tmp_qna_comment_list)
     }, 500)
   }, [])
 
   return (
     <>
+      <div className="hidden">{refresh}</div>
       <div className="mx-auto mt-8 flex rounded-lg" style={{ width: "650px" }}>
         <div className="w-full keep-all">
-          <Comment />
+          <Comment qna_comment_list={qna_comment_list} />
           <QuestionHistory qna_query_list={qna_query_list} />
         </div>
       </div>

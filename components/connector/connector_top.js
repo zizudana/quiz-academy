3d1b63693a7a95faa1f85ec8d1ee7e3ed72fe5ec
sehandev <sehandev@gmail.com>
2020-11-12 17:58:22 +0900

    ADD : connector DB 연결

--- components/connector/connector_top.js
+++ components/connector/connector_top.js
@@ -2,48 +2,40 @@ import LockedSVG from "../svg/lock"
 import UnlockedSVG from "../svg/unlock"
 
 import Link from "next/link"
-import { useState } from "react"
 
-const ConnectorTop = () => {
-  const [is_open, set_is_open] = useState(false)
-
-  const toggle_is_open = () => {
-    if (is_open) {
-      set_is_open(false)
-    } else {
-      set_is_open(true)
-    }
-  }
-
+const QnaPanel = ({ qna_object }) => {
   // default state : closed
   let card_opacity = "0.6"
   let message_color = "text-red-600"
   let message_content = "Closed"
+  let pointer_events = "none"
 
   // changed state : open
-  if (is_open) {
+  if (qna_object.isopen === "true") {
     card_opacity = "1.0"
     message_color = "text-green-600"
     message_content = "Open"
+    pointer_events = ""
   }
 
   const LockSVG = () => {
-    if (is_open) {
+    if (qna_object.isopen === "true") {
       return <UnlockedSVG className="fill-current w-4 h-4 mr-2" />
     }
     return <LockedSVG className="fill-current w-4 h-4 mr-2" />
   }
-
   return (
     <>
       {/* Horizontal Card */}
-      <div className="mx-auto mt-8 flex shadow-lg rounded-lg" style={{ width: "650px", opacity: card_opacity, height: "18rem" }}>
+      <div
+        className="mx-auto mt-8 flex shadow-lg rounded-lg"
+        style={{ width: "650px", opacity: card_opacity, height: "18rem", pointerEvents: pointer_events }}
+      >
         {/* 왼쪽 사진 */}
         <div
           className="border border-r-0 border-gray-400 h-auto flex-none bg-cover bg-no-repeat bg-center rounded-l-lg overflow-hidden"
           style={{ width: "12rem", backgroundImage: 'url("/img/card_image_test_1.jpg")' }}
           title="실시간 질의응답 썸네일"
-          onClick={toggle_is_open}
         />
 
         {/* 오른쪽 패널 */}
@@ -53,12 +45,12 @@ const ConnectorTop = () => {
               <LockSVG />
               {message_content}
             </p>
-            <Link href="https://us02web.zoom.us/j/5015530171?pwd=aDg2MFZEdzU4YnNYYjVHazBGd1BBUT09">
-              <a className="text-gray-900 font-bold text-xl hover:text-green-600">{"오렌지 화학 Q&A"}</a>
+            <Link href={qna_object.zoomlink}>
+              <a className="text-gray-900 font-bold text-xl hover:text-green-600">{qna_object.title.replaceAll("_", " ")}</a>
             </Link>
-            <p className="text-gray-900 mb-2">김철수 선생님</p>
-            <p className="text-gray-700 text-base mb-2">내용 미정</p>
-            <p className="text-gray-700 text-base">매주 금요일 PM 09 : 00 ~ PM 10 : 30</p>
+            <p className="text-gray-900 mb-2">{`${qna_object.teachername} 선생님`}</p>
+            <p className="text-gray-700 text-base mb-2">{qna_object.content}</p>
+            <p className="text-gray-700 text-base">{qna_object.operatingtime}</p>
           </div>
         </div>
       </div>
@@ -66,4 +58,4 @@ const ConnectorTop = () => {
   )
 }
 
-export default ConnectorTop
+export default QnaPanel
--- pages/user/connector.js
+++ pages/user/connector.js
@@ -1,14 +1,104 @@
+import RefreshSVG from "../../components/svg/refresh"
 import Layout from "../../components/layout/layout_user"
 import ConnectorTop from "../../components/connector/connector_top"
 import ConnectorBottom from "../../components/connector/connector_bottom"
 
-const ConnectorPage = () => {
+import { useSession } from "next-auth/client"
+import { useState, useEffect } from "react"
+const axios = require("axios")
+
+const ConnectorPage = ({ rest_api_url }) => {
+  const [refresh, set_refresh] = useState(0) // 새로고침
+  const [qna_list, set_qna_list] = useState([]) // 학생 목록
+  const [session, loading] = useSession()
+
+  if (session && !loading) {
+    useEffect(() => {
+      let tmp_qna_list = []
+      axios
+        .get(`${rest_api_url}/qnastudents/all/studentid/${session.user.image}`)
+        .then(function (response) {
+          // handle success
+
+          /**
+           * @param {Object} qna_student
+           * * @param {String} _id (본인) ID
+           * * @param {String} qnaid (부모) 질의응답 ID
+           * * @param {String} studentid (부모) 학생 ID
+           * * @param {String} studentname 학생 이름
+           * * @param {Array} qnaquerylist 상시 질문 목록
+           * * @param {Array} qnacommentlist 코멘트 목록
+           */
+          let tmp_qna_student_list = response.data.qna_student_arr
+          tmp_qna_student_list.sort((a, b) => a._id.localeCompare(b._id))
+
+          tmp_qna_student_list.map((qna_student_object) => {
+            axios
+              .get(`${rest_api_url}/qnas/${qna_student_object.qnaid}`)
+              .then(function (response) {
+                /**
+                 * @param {Object} qna_object
+                 * * @param {String} _id (본인) ID
+                 * * @param {String} teacherid (부모) 교사 ID
+                 * * @param {String} title 제목
+                 * * @param {String} content 내용
+                 * * @param {String} operatingtime 운영 시간
+                 * * @param {String} isopen 운영 여부
+                 * * @param {String} zoomlink Zoom 링크
+                 * * @param {String} content 내용
+                 * * @param {Array} qnastudentlist 질의응답+학생 목록
+                 * * @param {Array} qnafeedbacklist 피드백 목록
+                 */
+                let qna_object = response.data
+                tmp_qna_list.push(qna_object)
+              })
+              .catch(function (error) {
+                // handle error
+                console.log(error)
+              })
+          })
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error)
+        })
+
+      setTimeout(() => {
+        set_qna_list(tmp_qna_list)
+      }, 500)
+    }, [])
+  }
+
   return (
     <Layout>
-      <ConnectorTop />
-      <ConnectorBottom />
+      {qna_list.map((qna_object, index) => {
+        return (
+          <div key={"connector-" + index}>
+            {/* 새로고침 */}
+            <div className="mx-auto mt-8 flex flex-row-reverse" style={{ width: "650px" }}>
+              <button className="w-6 h-6 rounded-full p-1 outline-none border border-green-500" onClick={() => set_refresh(refresh + 1)}>
+                <RefreshSVG fill="#48bb78" />
+              </button>
+            </div>
+
+            {/* Top */}
+            <ConnectorTop qna_object={qna_object} rest_api_url={rest_api_url} />
+
+            {/* Bottom */}
+            <ConnectorBottom qna_object={qna_object} rest_api_url={rest_api_url} />
+          </div>
+        )
+      })}
     </Layout>
   )
 }
+
+const getServerSideProps = async () => {
+  const rest_api_url = process.env.REST_API_URL
+
+  return { props: { rest_api_url } }
+}
+
+export { getServerSideProps }
 
 export default ConnectorPage
